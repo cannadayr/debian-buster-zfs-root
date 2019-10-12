31a5a6ca192114fcf739ba95f9a6db66f313634f
Rowan Cannaday <cannadayr@gmail.com>
2019-10-12 15:51:01 -0400

    boot, dev/proc/sys mounts

--- mount.sh
+++ mount.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+DISK=mydisk
+
 mount -t zfs vpool/ROOT/debian-sid /mnt/
+mount /dev/disk/by-id/$DISK-part2 /boot/
 zfs list | grep ^vpool | awk ' ~ legacy {print }' | sed '1d;2d;3d' | sed 's/vpool\///g' | xargs -I{} echo mount -t zfs vpool/{} /mnt/{}
+
+mount --rbind /dev /mnt/dev
+mount --rbind /proc /mnt/proc
+mount --rbind /sys /mnt/sys
